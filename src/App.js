bc02c323a8ef88fd665721c659eb4b9173a70bb9
ericmtodd <eterictodd@gmail.com>
2021-12-20 21:27:58 -0700

    Tasks can be added, but do not persist across refreshes.

--- src/App.js
+++ src/App.js
@@ -5,13 +5,38 @@ import Overview from './components/Overview'
 class App extends Component {
   constructor() {
     super()
+    this.state = {
+      tasks: [],
+      value: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    let newTasksList = this.state.tasks
+    newTasksList.push(this.state.value)
+    this.setState({tasks: newTasksList})
+    this.setState({value: ''})
+    event.preventDefault();
   }
 
   render() {
-    this.state = {tasks: ['Finish this app', 'Fuck bitches, get money']}
-
     return (
       <div className="app">
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            <strong>Add a task:</strong>
+            <br />
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
         <Overview tasks={this.state.tasks}/>
       </div>
     )
